27da27dacb6cb0db47b4fda9e86aa262207cb99d
NikEurope <nik.europe@gmail.com>
2016-06-23 15:12:14 +0200

    4

---
+++ JSONPHP/Euro2016/index.php
@@ -0,0 +1,76 @@
+
+
+<!-- Exercice PHP Euro2016  PHP JSON -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+
+<?php
+
+// ------------------------  json
+
+$euro = file_get_contents("competition.json");
+$content = json_decode($euro);
+?>
+<br />
+<br />
+<h1><?php echo "POE ".$content->name; ?></h1>
+<hr>
+<?php
+for ($i=0; $i < count($content->groups); $i++)
+{
+$groupes = $content->groups[$i]->id;
+?>
+<h3><a href = ""><?php echo "Groupre : " .$groupes;?></a><br /></h3>
+<?php
+for ($i2=0; $i2 < count($content->groups[$i]->teams) ; $i2++)
+{
+$teams = $content->groups[$i]->teams[$i2];
+?>
+<a href = ""><?php echo " $teams <br />"?></a>
+<?php
+}
+}
+?>
+
+<br /><br />
+<hr>
+<br /><br />
+
+<!-- STYLE ---------------------------------------->
+
+<style>
+body{
+  background-color: #ccddff;
+}
+
+h1{
+  color: #002266;
+}
+
+h3{
+  color: #476b6b;
+  font-family: sans-serif;
+}
+
+a:link {
+    text-decoration: none;
+    color: #476b6b;
+}
+
+a:visited {
+    text-decoration: none;
+    color: #476b6b;
+}
+
+a:hover{
+  text-decoration: underline;
+  color: red;
+}
+</style>
+</body>
+</html>
