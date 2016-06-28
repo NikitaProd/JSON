da22ea7efa99e6e364154cab7bc377e1c40afdee
NikEurope <nik.europe@gmail.com>
2016-06-28 09:59:30 +0200

    4

--- JSONPHP/Euro2016drap/index.php
+++ JSONPHP/Euro2016drap/index.php
@@ -16,19 +16,21 @@
 $euro = file_get_contents("competition.json");
 $content = json_decode($euro);
 ?>
-<br />
+
 <br />
 <!-- EURO -->
 <h1><?php echo "POE ".$content->name; ?></h1>
 <hr>
 
 <!-- GROUPS -->
+
 <?php
 for ($i=0; $i < count($content->groups); $i++)
 {
 $groupes = $content->groups[$i]->id;
 ?>
-<h3><a href = ""><?php echo "Groupre : " .$groupes;?></a><br /></h3>
+<h3><br /><br /><a href = ""><?php echo "Groupre : " .$groupes;?></a><br /></h3>
+
 
 <!-- TEAMS -->
 <?php
@@ -36,17 +38,41 @@ for ($i2=0; $i2 < count($content->groups[$i]->teams) ; $i2++)
 {
 $teams = $content->groups[$i]->teams[$i2];
 ?>
-<a href = ""><?php echo " $teams <br />"?></a>
+
+<!-- NOM 1-->
 <?php
+for ($i3=0; $i3 < count($content->groups[$i]->teams[$i2]->nom) ; $i3++)
+{
+$nom = $content->groups[$i]->teams[$i2]->nom;
+?>
+
+<?php
+for ($i4=0; $i4 < count($content->groups[$i]->teams[$i2]->flag) ; $i4++)
+{
+$flag = $content->groups[$i]->teams[$i2]->flag;
+?>
+
+
+<div class="nom"><a href = ""><?php echo $nom ?></a> -
+  <a href = ""><img src="<?php echo $flag ?>" border="1" />
+</div><br />
+
+<?php
+}
+}
 }
 }
 ?>
 
-<br /><br />
 
 <br />
+<br />
+<br />
+
+<!-- #6 Les arrays  Profils-->
 
 
+<br /><br />
 <hr>
 <br /><br />
 
@@ -80,6 +106,18 @@ a:hover{
   text-decoration: underline;
   color: red;
 }
+
+img {
+    height: 25px;
+    width: 25px;
+    margin: 0,4%;
+    border-radius: 50%;
+}
+
+.nom{
+  font-size: 20px;
+}
+
 </style>
 </body>
 </html>
